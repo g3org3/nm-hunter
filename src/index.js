e0341c37563b3f7500d1e3519ce2a8aa407c8f32
Jose Albizures <albizures3601@gmail.com>
2018-01-21 21:46:54 -0600

    refactor(general): make nm-hunter cross-platform  make the search and the size calculation be cross-platform

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 #! /usr/bin/env node
 
+require('util.promisify').shim()
+require('array-includes').shim()
+
 const program = require('commander')
 const { version } = require('../package.json')
 const { nmHunter } = require('./methods')
 
 program.version(version).option('-v, --version', 'output the version number')
+program.option('-s, --sort', 'sort the node_modules in ASC')
+program.option('-w --warning', 'only display node_modules above 99M')
 program.parse(process.argv)
 
 // Search for node_modules
-nmHunter()
+nmHunter({ warning: program.warning, sort: program.sort })
 
 // catch ctrl+c event and exit normally
-process.on('SIGINT', function () {
-  console.log(' Exiting gracefully ...')
-})
+// process.on('SIGINT', function () {
+//   console.log(' Exiting gracefully ...')
+// })
